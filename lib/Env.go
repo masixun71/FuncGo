e5b904c0a4cb93b419b2077a3a47e11cb0542636
马斯珣 <masixun@baijiahulian.com>
2017-08-02 11:20:06 +0800

    add func getProjectRoot

--- lib/Env.go
+++ lib/Env.go
@@ -7,35 +7,47 @@ import (
 
 var root string
 
-func GetRoot() string  {
+func GetRoot() string {
 	if root != "" {
 		return root
 	}
 
 	roots, _ := os.Getwd()
-	index := strings.LastIndex(roots, "FuncGo")
-	root = string([]rune(roots)[:index + 6])
 
-	return root
+	strPath := strings.Split(roots, "/")
+	projectPath := ""
+	lenPath := len(strPath)
+
+	for i := 0; i < lenPath-1; i++ {
+		if strPath[i] == "go" && strPath[i+1] == "src" {
+			if i+2 <= lenPath-1 {
+				projectPath = strings.Join(strPath[0:i+3], "/")
+			} else {
+				panic("your project not in go/src")
+			}
+		}
+	}
+
+	if projectPath == "" {
+		panic("your project not in go/src")
+	}
+	root = projectPath
+
+	return projectPath
 }
 
 type Path interface {
 	MakePath() string
 }
 
-
 type Pather struct {
 	path string
 }
 
-
 func NewPath(path string) Path {
-	return &Pather{path:path}
+	return &Pather{path: path}
 }
 
-
-func (p *Pather) MakePath() string{
+func (p *Pather) MakePath() string {
 	return GetRoot() + p.path
 }
-
-
--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"FuncGo/Func"
+	"FuncGo/lib"
+	"fmt"
 )
 
 func main() {
 
 	//InvokeObjectMethod(new(YourT2), "MethodFoo", 10, "abc")
 
-	makeFiler := Func.NewMakeFilerSimple("/Func/ast.go", "MaxTF", Func.TypeT)
+	//makeFiler := Func.NewMakeFilerSimple("/Func/ast.go", "MaxTF", Func.TypeT)
 	//makeFiler.MakeMethod(new(Func.ValueS))
-	makeFiler.MakeFunc()
+	//makeFiler.MakeFunc()
 }
