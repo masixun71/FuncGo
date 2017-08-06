c6f840a8bb90081ec213e917ec2abec5b565934f
马斯珣 <masixun@baijiahulian.com>
2017-08-06 20:59:20 +0800

    add test

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"go/token"
 	"go/parser"
 	"go/ast"
+	"io/ioutil"
+	"strings"
+	"fmt"
 )
 
 func main() {
@@ -16,17 +19,26 @@ func main() {
 	fset := token.NewFileSet()
 	f, _ := parser.ParseFile(fset, path.GetPathByRoot(), nil, parser.ParseComments)
 
+	b, _ := ioutil.ReadFile(path.GetPathByRoot())
 
 	for _, decl := range f.Decls {
 		fn, ok := decl.(*ast.FuncDecl)
 		if ok && fn.Name.Name == "CompareTF" {
+
+			str := string(b[fn.Type.Func-1: fn.Body.Rbrace])
+			//var buffer bytes.Buffer
 			for _, field := range fn.Type.Params.List {
 				for _, name := range field.Names {
-					name.Obj.Name = "aa"
+					//varLen := len(name.Name)
+					index := strings.Index(str, name.Name)
+					str = str[0:index] + "p" + str[index:len(str)]
+
 				}
 			}
+
+			fmt.Println(str)
+
 		}
 	}
 
-	ast.Print(fset, f)
 }
